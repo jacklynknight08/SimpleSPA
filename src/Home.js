9684a9aa5674d8831b05f34ad3a65576406a2e43
jacklynknight08 <jnk08@outlook.com>
2018-01-04 13:02:57 -0600

    Creating Home and Stuff components

---
+++ src/Home.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return(
+            <div>
+                <h2>Hello</h2>
+                <p>Lorem ipsum shit. Cras facilisis urna ornare ex volutpat, et
+                    convallis erat elementum. Ut aliquam, ipsum vitae
+                    gravida suscipit, metus dui bibendum est, eget rhoncus nibh
+                    metus nec massa. Maecenas hendrerit laoreet augue
+                    nec molestie. Cum sociis natoque penatibus et magnis
+                    dis parturient montes, nascetur ridiculus mus.
+                </p>
+                <p>
+                    More stuff placed here.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Home;
