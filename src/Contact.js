4cef2332dff028b108e5d97e78ba05a4eedf4816
jacklynknight08 <jnk08@outlook.com>
2018-01-04 13:05:58 -0600

    Contact page component

---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return(
+            <div>
+                <h2>Questions?</h2>
+                <p>The easiest thing to do is post on our forum</p>
+            </div>
+        );
+    }
+}
+
+export default Contact;
