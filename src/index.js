251384222cd557db1231e46c69395a99b4cd2d83
jacklynknight08 <jnk08@outlook.com>
2018-01-04 12:41:07 -0600

    Starting from scratch

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import ReactDom from 'react-dom';
+import Main from './Main';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDom.render(
+    <Main/>,
+    document.getElementById("root")
+);
