4ccc7714e17ef99d9af2e8aa3d953109ce693e80
jacklynknight08 <jnk08@outlook.com>
2018-01-07 15:05:06 -0600

    Defined routing region

--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
+import { Route, NavLink, HashRouter } from 'react-router-dom';
+import Home from './Home';
+import Stuff from './Stuff';
+import Contact from './Contact';
 
+// Define routing region inside render method ('HashRouter')
+// This will provide foundation for the navigation and browsing history handling
+// NavLink component will replace <a> elements, value defined by 'to' prop
+// Route component will match URL with the correct content
+// When route is active, component specified by component prop will be rendered
 class Main extends Component {
     render() {
         return(
-            <div>
-                <h1>Simple SPA</h1>
-                <ul className="header">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/stuff">Stuff</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                </ul>
-                <div className="content">
-
+            <HashRouter>
+                <div>
+                    <h1>Simple SPA</h1>
+                    <ul className="header">
+                        <li><NavLink to="/">Home</NavLink></li>
+                        <li><NavLink to="/stuff">Stuff</NavLink></li>
+                        <li><NavLink to="/contact">Contact</NavLink></li>
+                    </ul>
+                    <div className="content">
+                        <Route path="/" component={Home}/>
+                        <Route path="/stuff" component={Stuff}/>
+                        <Route path="/contact" component={Contact}/>
+                    </div>
                 </div>
-            </div>
+            </HashRouter>
         );
     }
 }
